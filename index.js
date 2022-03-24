b80d72b59f81c00ef764ed0f4ba865ced476f239
Sandrine <sandrine.thominet@gmail.com>
2022-03-24 13:57:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+  }
+  
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+  }
+  
+function distanceTravelledInFeet (pickupLocation, dropOffLocation) {  
+    return (Math.abs(dropOffLocation - pickupLocation)) * 264;
+  }
+
+function calculatesFarePrice (pickupLocation, dropOffLocation) {
+      if ( distanceTravelledInFeet (pickupLocation, dropOffLocation) <= 400 )
+      { return 0;}
+      else if ( distanceTravelledInFeet (pickupLocation, dropOffLocation) <= 2000 && distanceTravelledInFeet (pickupLocation, dropOffLocation) > 400 )
+      { return ( distanceTravelledInFeet (pickupLocation, dropOffLocation) - 400 ) * 0.02;}
+      else if ( distanceTravelledInFeet (pickupLocation, dropOffLocation) > 2000 && distanceTravelledInFeet (pickupLocation, dropOffLocation) <= 2500 )
+      { return 25;}
+      else if ( distanceTravelledInFeet (pickupLocation, dropOffLocation) > 2500 )
+      { return 'cannot travel that far';}
+  }
